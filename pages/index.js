4138d1237e7d816a3947ea190a873b4ec9fe5e39
Leo Garza <lgarza@equibrand.com>
2021-12-02 16:53:46 -0600

    added meta data

--- pages/index.js
+++ pages/index.js
@@ -8,15 +8,20 @@ export default function Home() {
   
   return (
     <main>
+
+      {/* Checking if the user has logged in and passing that information inside the hero component */}
       <Hero session= {session}/>
+
+
+      {/* If user is logged in then we will show them the rest of the page, else it will be hidden and not rendered */}
     {
       session && (
         <>
-      <FeaturedBrands/>
-      <Features/>
-      <FeaturedNew/>
-      <FeaturedVideo/>
-      </>
+          <FeaturedBrands/>
+          <Features/>
+          <FeaturedNew/>
+          <FeaturedVideo/>
+        </>
       )
     }
 
