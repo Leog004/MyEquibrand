3e8c9ba499ed3f340cee9bb8ef0750c64b402182
Leo Garza <lgarza@equibrand.com>
2021-12-05 19:59:23 -0600

    adding changes and auth on signin

--- pages/Catelog/index.js
+++ pages/Catelog/index.js
@@ -1,10 +1,29 @@
 import React, {useState} from 'react'
+import { useSession } from 'next-auth/react'
 import { PDFReader, SideBar } from '../../components'
+import { useRouter } from 'next/router'
 export default function Catelog() {
 
     // Using the state component as a global variable that will be used to 
     // share information from sideBar and PDFREADER
     const [outSideNumber, setoutSideNumber] = useState(0)
+    const router = useRouter()
+
+        // Checking whether the user is logged in or not. 
+        const { data: session, status } = useSession({
+          required: true,
+          onUnauthenticated() {
+            // The user is not authenticated, handle it here.
+            // Since the user is not logged in. We are going to redirect them to the signIn Page
+            return router.push(
+                  '/auth/signIn'
+              )
+          }
+      })
+
+      if( status === "loading" || !session) {
+        return <h1>Loading...</h1>
+    }
 
   return (
     <div className='flex overflow-hidden'>
