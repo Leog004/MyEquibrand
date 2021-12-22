40a422299cdce0b9f71945d76c8aff6908a469c1
Leo Garza <lgarza@equibrand.com>
2021-12-22 10:58:55 -0600

    adding react-toastify and progress bar to image download

--- components/iShare/BrandGrid.jsx
+++ components/iShare/BrandGrid.jsx
@@ -1,27 +1,39 @@
-import React, {useEffect} from 'react'
+import React, {useState} from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDownload } from '@fortawesome/free-solid-svg-icons';
 import JSZip from "jszip";
 import { saveAs } from 'file-saver';
 
-export default function BrandGrid({title, handleChange,progress, setProgress}) {
 
-    useEffect(() => {
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+toast.configure();
 
-    },[])
+
+export default function BrandGrid({title, handleChange}) {
+
+    const toastId = React.useRef(null);
+
+    const [buttonClicked, setbuttonClicked] = useState(false);
+
+    // const Message = () => {
+        
+    //     toast.warn('The download is currently underway. Please stay on this page until the download is finished.', {position: toast.POSITION.BOTTOM_CENTER, autoClose: false});
+    // }
 
     async function download() {
 
        let zip = new JSZip();
        let folder = zip.folder('collection'); 
 
+       //Message();
+
         const data = await fetch('/api/readerZip?' + new URLSearchParams({
             brand: title,
         })).then((res) => res.json())  
-        
-        console.log(progress);
+            
+   
 
-        
         data.files.map((el, index) => {
           
             const imageBlob = fetch(`http://equibrand.com/Product Images/Brands/${title}/${el}`).then(response => response.blob());
@@ -29,21 +41,37 @@ export default function BrandGrid({title, handleChange,progress, setProgress}) {
             
         })
 
+        
         folder.generateAsync({ 
             type: "blob",     
             compression: "DEFLATE",
             compressionOptions: {
             level: 1
         }}, function updateCallback(metadata){
+
+
+                // check if we already displayed a toast
+                if(toastId.current === null){
+                    toastId.current = toast.warn(`The download is currently underway for ${title} images. Please stay on this page until the download is finished.`, {
+                        progress: (0 / 100),
+                        position: toast.POSITION.BOTTOM_CENTER,
+                        autoClose: false,
+                        closeOnClick: false
+                    });
+                } else {
+                    toast.update(toastId.current, {
+                        progress: metadata.percent.toFixed(2) / 100
+                    })
+                }
                 //console.log("progression: " + metadata.percent.toFixed(2) + " %")
 
-                setProgress(metadata.percent.toFixed(2));
+                //setProgress(metadata.percent.toFixed(2));
                 if(metadata.currentFile) {
                    // console.log("current file = " + metadata.currentFile);
                 }    
         }).then(content =>{ 
             saveAs(content, "files")
-            setProgress(0);
+            //setProgress(0);
         });
 
     }
@@ -65,18 +93,21 @@ export default function BrandGrid({title, handleChange,progress, setProgress}) {
         </article>
             {/* <!-- BOTTOM ICONS --> */}
             <div className="absolute py-2 bottom-6 left-0 right-0 flex justify-center z-40">
-            <div className="rounded-full p-2 bg-gray-700 text-white mr-2 cursor-pointer hover:bg-gray-800 transition-all duration-150 ease-linear">
-                <button onClick={download}>
-                    <FontAwesomeIcon className='h-8 w-8' icon={faDownload} />
-                </button>
+                <div className={`rounded-full p-2 bg-gray-700 text-white mr-2 cursor-pointer hover:bg-gray-800 transition-all duration-150 ease-linear ${buttonClicked && 'opacity-40'}`}>
+                    <button onClick={ () => {
+                       !buttonClicked && download();
+                       setbuttonClicked(true);
+                    } }>
+                        <FontAwesomeIcon className='h-8 w-8' icon={faDownload} />
+                    </button>
+                </div>
             </div>
-        </div>
-        {
+        {/* {
             (progress > 1 ) && 
             <div className="w-full bg-gray-200 rounded-full">
                 <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-l-full" style={{width: `${ progress}%`}}> { progress}%</div>
             </div>
-        }
+        } */}
 
     </div>
     )
